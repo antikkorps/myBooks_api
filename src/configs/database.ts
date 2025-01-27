0ea32588c19a0baa9bb055258454b288bcfb302c
Franck Vienot <franckvienot7@gmail.com>
2025-01-27 14:48:03 +0100

    add neon adress to env

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,31 +1,30 @@
-import dotenv from 'dotenv';
-import { Sequelize } from 'sequelize-typescript';
-import { Book } from '../models/book.js';
-import { BookReadingList } from '../models/bookReadingList.js';
-import { ReadingList } from '../models/readingList.js';
-import { User } from '../models/user.js';
+import dotenv from "dotenv"
+import { Sequelize } from "sequelize-typescript"
+import { Book } from "../models/book.js"
+import { BookReadingList } from "../models/bookReadingList.js"
+import { ReadingList } from "../models/readingList.js"
+import { User } from "../models/user.js"
 
-const result = dotenv.config();
- 
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
+const result = dotenv.config()
+
+if (!process.env.DB_URL) {
+  throw new Error("La variable d'environnement DB_CONNECTION_URL n'est pas définie !")
+}
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
   models: [Book, ReadingList, BookReadingList, User], // Ajout automatique des modèles
-  logging: false  
+  logging: false,
 })
-
 
 export const initDatabase = async () => {
   try {
     await sequelize.authenticate()
-    console.log('Connection à la base de données établie.')
+    console.log("Connection à la base de données établie.")
     await sequelize.sync({ alter: true }) // En développement uniquement
-    console.log('Modèles synchronisés avec la base de données.')
+    console.log("Modèles synchronisés avec la base de données.")
   } catch (error) {
-    console.error('Impossible de se connecter à la base de données:', error)
+    console.error("Impossible de se connecter à la base de données:", error)
     process.exit(1)
   }
 }
